d9dfb451a7fbc93e02f40461fc79dbf5055b101b
Josh Bassett <josh.bassett@gmail.com>
2017-03-08 21:18:44 +1100

    Change the graph data structure to support key-value

--- src/Data/Graph.purs
+++ src/Data/Graph.purs
@@ -1,12 +1,19 @@
 module Data.Graph
   ( AdjacencyList
   , Graph
+  , fromAdjacencyList
   , empty
   , isEmpty
-  , fromAdjacencyList
+  , insertVertex
+  , updateVertex
+  , deleteVertex
+  , insertEdge
+  , deleteEdge
+  , keys
   , vertices
   , size
-  , elem
+  , lookup
+  , member
   , adjacent
   , adjacent'
   , isAdjacent
@@ -15,11 +22,8 @@ module Data.Graph
   , shortestPath'
   , traverse
   , connectedComponents
-  , insertVertex
-  , insertEdge
-  , deleteVertex
-  , deleteEdge
   , filter
+  , update
   ) where
 
 import Prelude
@@ -28,9 +32,8 @@ import Data.Foldable (elem, foldl) as F
 import Data.List (List(..), (\\), (:))
 import Data.List (filter, reverse, singleton, snoc) as L
 import Data.Map (Map)
-import Data.Map (alter, delete, empty, insert, isEmpty, keys, lookup, member, singleton, size, toList) as M
+import Data.Map (alter, delete, empty, insert, isEmpty, keys, lookup, member, singleton, size, toList, update, values) as M
 import Data.Maybe (Maybe(..), maybe, fromJust)
-import Data.Newtype (class Newtype, wrap, unwrap, over)
 import Data.Set (Set)
 import Data.Set (empty, insert, member) as S
 import Data.Tuple (Tuple(..), fst, snd)
@@ -40,84 +43,129 @@ import Data.PQueue (PQueue)
 import Data.PQueue (insert, isEmpty, singleton) as PQ
 import Data.PQueue.Partial (head, tail) as PPQ
 
--- | `Graph a w` represents a graph of vertices of type `a` connected by edges
--- | with a weight of type `w`. It is represented internally as a map of maps.
-newtype Graph a w = Graph (Map a (Map a w))
+-- | `Graph k w v` represents a graph of vertices of type `v` with keys of
+-- | type `k`, connected by edges with a weight of type `w`.
+data Graph k w v = Graph (Map k v) (Map k (Map k w))
 
--- | `AdjacencyList a w` represents a `List` of vertices of type `a` with a
+-- | `AdjacencyList k v w` represents a `List` of vertices of type `k` with a
 -- | list of adjacent vertices connected with edges of type `w`.
-type AdjacencyList a w = List (Tuple a (List (Tuple a w)))
+type AdjacencyList k v w = List (Tuple k (Tuple v (List (Tuple k w))))
 
-derive instance newtypeGraph :: Newtype (Graph a w) _
+instance showGraph :: (Show k, Show v, Show w) => Show (Graph k w v) where
+  show (Graph vertexMap edgeMap) = "Graph vertices: " <> show vertexMap <> " edges: " <> show edgeMap
 
-instance showGraph :: (Show a, Show w) => Show (Graph a w) where
-  show = show <<< unwrap
-
--- | An empty graph.
-empty :: forall a w. (Ord a) => Graph a w
-empty = wrap M.empty
-
--- | Test whether a graph is empty.
-isEmpty :: forall a w. (Ord a) => Graph a w -> Boolean
-isEmpty = M.isEmpty <<< unwrap
+instance functorGraph :: Functor (Graph k w) where
+  map f (Graph vertexMap edgeMap) = Graph (map f vertexMap) edgeMap
 
 -- | Create a graph from an adjacency list.
-fromAdjacencyList :: forall a w. (Ord a) => AdjacencyList a w -> Graph a w
-fromAdjacencyList as = insertEdges $ insertVertices empty
+fromAdjacencyList :: forall k v w. Ord k => AdjacencyList k v w -> Graph k w v
+fromAdjacencyList as = (insertEdges <<< insertVertices) empty
   where
     -- Unwrap the vertices from the adjacency list and insert them into the graph.
-    insertVertices g = F.foldl (\m (Tuple a _) -> insertVertex a m) g as
+    insertVertices :: Graph k w v -> Graph k w v
+    insertVertices g = F.foldl (flip unwrapVertex) g as
+
+    unwrapVertex :: Tuple k (Tuple v (List (Tuple k w))) -> Graph k w v -> Graph k w v
+    unwrapVertex (Tuple k (Tuple v _)) = insertVertex k v
 
     -- Unwrap the edges from the adjacency list and insert them into the graph.
-    insertEdges g = F.foldl unwrapEdges g as
-    unwrapEdges g (Tuple a edges) = F.foldl (flip $ unwrapEdge a) g edges
+    insertEdges :: Graph k w v -> Graph k w v
+    insertEdges g = F.foldl (flip unwrapEdges) g as
+
+    unwrapEdges :: Tuple k (Tuple v (List (Tuple k w))) -> Graph k w v -> Graph k w v
+    unwrapEdges (Tuple a (Tuple v edges)) g = F.foldl (flip $ unwrapEdge a) g edges
+
+    unwrapEdge :: k -> (Tuple k w) -> Graph k w v -> Graph k w v
     unwrapEdge a (Tuple b w) = insertEdge a b w
 
+-- | An empty graph.
+empty :: forall k v w. Graph k w v
+empty = Graph M.empty M.empty
+
+-- | Test whether a graph is empty.
+isEmpty :: forall k v w. Ord k => Graph k w v -> Boolean
+isEmpty (Graph vertexMap _) = M.isEmpty vertexMap
+
+-- | Insert a vertex into a graph.
+insertVertex :: forall k v w. Ord k => k -> v -> Graph k w v -> Graph k w v
+insertVertex key value (Graph vertexMap edgeMap) = Graph (M.insert key value vertexMap) edgeMap
+
+-- | Update a vertex in a graph.
+updateVertex :: forall k v w. Ord k => k -> v -> Graph k w v -> Graph k w v
+updateVertex key value (Graph vertexMap edgeMap) = Graph (M.update updateVertex' key vertexMap) edgeMap
+  where updateVertex' _ = Just value
+
+-- | Delete a vertex from a graph.
+deleteVertex :: forall k v w. Ord k => k -> Graph k w v -> Graph k w v
+deleteVertex vertex = (deleteVertex' <<< deleteEdges <<< deleteIncidentEdges)
+  where
+    deleteVertex' (Graph vertexMap edgeMap) = Graph (M.delete vertex vertexMap) edgeMap
+    deleteEdges (Graph vertexMap edgeMap) = Graph vertexMap (M.delete vertex edgeMap)
+    deleteIncidentEdges graph = F.foldl (\g v -> deleteEdge v vertex g) graph (adjacent vertex graph)
+
+-- | Insert an edge into a graph.
+insertEdge :: forall k v w. Ord k => k -> k -> w -> Graph k w v -> Graph k w v
+insertEdge from to weight (Graph vertexMap edgeMap) = Graph vertexMap (M.alter insertEdge' from edgeMap)
+  where insertEdge' = maybe (Just $ M.singleton to weight) (\weightMap -> Just $ M.insert to weight weightMap)
+
+-- | Delete an edge from a graph.
+deleteEdge :: forall k v w. Ord k => k -> k -> Graph k w v -> Graph k w v
+deleteEdge from to (Graph vertexMap edgeMap) = Graph vertexMap (M.alter deleteEdge' from edgeMap)
+  where deleteEdge' = maybe Nothing (\weightMap -> Just $ M.delete to weightMap)
+
+-- | Get the keys of a graph.
+keys :: forall k v w. Graph k w v -> List k
+keys (Graph vertexMap _) = M.keys vertexMap
+
 -- | Get the vertices of a graph.
-vertices :: forall a w. Graph a w -> List a
-vertices = M.keys <<< unwrap
+vertices :: forall k v w. Graph k w v -> List v
+vertices (Graph vertexMap _) = M.values vertexMap
 
 -- | Get the number of vertices in a graph.
-size :: forall a w. Graph a w -> Int
-size = M.size <<< unwrap
+size :: forall k v w. Graph k w v -> Int
+size (Graph vertexMap _) = M.size vertexMap
 
--- | Test whether a vertex is in a graph.
-elem :: forall a w. (Ord a) => a -> Graph a w -> Boolean
-elem vertex = M.member vertex <<< unwrap
+-- | Get a vertex matching a key.
+lookup :: forall k v w. Ord k => k -> Graph k w v -> Maybe v
+lookup key (Graph vertexMap _) = M.lookup key vertexMap
 
--- | Get the adjacent vertices of a vertex.
-adjacent :: forall a w. (Ord a) => a -> Graph a w -> List a
-adjacent vertex graph = maybe Nil M.keys (M.lookup vertex (unwrap graph))
+-- | Test whether a key is in a graph.
+member :: forall k v w. Ord k => k -> Graph k w v -> Boolean
+member key (Graph vertexMap _) = M.member key vertexMap
+
+-- | Get the keys of the vertices adjacent to a key.
+adjacent :: forall k v w. Ord k => k -> Graph k w v -> List k
+adjacent key (Graph _ edgeMap)= maybe Nil M.keys (M.lookup key edgeMap)
 
 -- | Get the adjacent vertices and associated costs of a vertex.
-adjacent' :: forall a w. (Ord a) => a -> Graph a w -> List (Tuple a w)
-adjacent' vertex graph = maybe Nil M.toList (M.lookup vertex (unwrap graph))
+adjacent' :: forall k v w. Ord k => k -> Graph k w v -> List (Tuple k w)
+adjacent' key (Graph _ edgeMap)= maybe Nil M.toList (M.lookup key edgeMap)
 
 -- | Test whether two vertices are adjacent in a graph.
-isAdjacent :: forall a w. (Ord a) => a -> a -> Graph a w -> Boolean
-isAdjacent a b graph = maybe false (M.member b) (M.lookup a (unwrap graph))
+isAdjacent :: forall k v w. Ord k => k -> k -> Graph k w v -> Boolean
+isAdjacent a b (Graph _ edgeMap) = maybe false (M.member b) (M.lookup a edgeMap)
 
 -- | Get the weight of the edge between two vertices. Returns `Nothing` if no
 -- | edge exists between the vertices.
-weight :: forall a w. (Ord a) => a -> a -> Graph a w -> Maybe w
-weight from to graph = maybe Nothing (M.lookup to) (M.lookup from (unwrap graph))
+weight :: forall k v w. Ord k => k -> k -> Graph k w v -> Maybe w
+weight a b (Graph _ edgeMap) = maybe Nothing (M.lookup b) (M.lookup a edgeMap)
 
 -- | Get the shortest path between two vertices. Returns `Nothing` if no path
 -- | exists between the vertices.
-shortestPath :: forall a w. (Ord a, Ord w, Semiring w) => a -> a -> Graph a w -> Maybe (List a)
+shortestPath :: forall k v w. (Ord k, Ord w, Semiring w) => k -> k -> Graph k w v -> Maybe (List k)
 shortestPath from to = shortestPath' (_ == to) from
 
 -- | Get the shortest path from a starting vertex to a vertex that satisifes a
 -- | predicate function. Returns `Nothing` if no path exists between the
 -- | vertices.
-shortestPath' :: forall a w. (Ord a, Ord w, Semiring w) => (a -> Boolean) -> a -> Graph a w -> Maybe (List a)
+shortestPath' :: forall k v w. (Ord k, Ord w, Semiring w) => (k -> Boolean) -> k -> Graph k w v -> Maybe (List k)
 shortestPath' p start g = go (PQ.singleton zero start) S.empty (M.singleton start zero) M.empty
   where
-    go :: PQueue w a     -- priority queue of fringe vertices
-       -> Set a          -- set of visited vertices
-       -> Map a w        -- map from vertices to costs
-       -> Map a a        -- map from vertices to adjacent vertices
-       -> Maybe (List a) -- shortest path
+    go :: PQueue w k     -- priority queue of fringe vertices
+       -> Set k          -- set of visited vertices
+       -> Map k w        -- map from vertices to costs
+       -> Map k k        -- map from vertices to adjacent vertices
+       -> Maybe (List k) -- shortest path
     go fringe visited labels edges =
       if PQ.isEmpty fringe then Nothing
       else
@@ -136,24 +184,24 @@ shortestPath' p start g = go (PQ.singleton zero start) S.empty (M.singleton star
                  edges' = F.foldl (\a (Tuple v _) -> M.insert v vertex a) edges successors
              in go fringe'' visited' labels' edges'
 
-    successorsAndCosts :: a -> w -> List (Tuple a w)
+    successorsAndCosts :: k -> w -> List (Tuple k w)
     successorsAndCosts v cost = map (\(Tuple v c) -> Tuple v (cost + c)) (adjacent' v g)
 
-    findPath :: a -> Map a a -> List a
+    findPath :: k -> Map k k -> List k
     findPath vertex edges
       | M.member vertex edges =
         let vertex' = lookup' vertex edges
         in (findPath vertex' edges) <> L.singleton vertex
       | otherwise = L.singleton vertex
 
-    lookup' :: forall k v. (Ord k) => k -> Map k v -> v
+    lookup' :: forall k v. Ord k => k -> Map k v -> v
     lookup' k m = unsafePartial $ fromJust $ M.lookup k m
 
 -- | Perform a depth-frist traversal of a graph from a starting vertex.
 -- | Returns a `List` of the visited vertices.
-traverse :: forall a w. (Ord a) => a -> Graph a w -> List a
+traverse :: forall k v w. Ord k => k -> Graph k w v -> List k
 traverse from g
-  | elem from g =
+  | member from g =
     let go Nil path = path
         go (v:vs) path
           | F.elem v path = go vs path
@@ -163,37 +211,29 @@ traverse from g
 
 -- | Get the strongly connected components of a graph. Returns a `List` of
 -- | connected subgraphs.
-connectedComponents :: forall a w. (Ord a) => Graph a w -> List (Graph a w)
+connectedComponents :: forall k v w. Ord k => Graph k w v -> List (Graph k w v)
 connectedComponents g =
   let go Nil gs = gs
-      go (v:vs) gs =
-        let vs' = traverse v g
-            as = map (\a -> Tuple a (adjacent' a g)) vs'
-        in go (vs \\ vs') (L.snoc gs (fromAdjacencyList as))
-  in go (vertices g) Nil
+      go (k:ks) gs =
+        let ks' = traverse k g
+            as = map (\a -> Tuple a (Tuple (unsafePartial $ fromJust $ lookup a g) (adjacent' a g))) ks'
+        in go (ks \\ ks') (L.snoc gs (fromAdjacencyList as))
+  in go (keys g) Nil
 
--- | Insert a vertex into a graph.
-insertVertex :: forall a w. (Ord a) => a -> Graph a w -> Graph a w
-insertVertex vertex = over Graph $ M.insert vertex M.empty
-
--- | Insert an edge into a graph.
-insertEdge :: forall a w. (Ord a) => a -> a -> w -> Graph a w -> Graph a w
-insertEdge from to w = over Graph $ M.alter insertEdge' from
-  where insertEdge' = maybe Nothing (Just <<< M.insert to w)
-
--- | Delete a vertex from a graph.
-deleteVertex :: forall a w. (Ord a) => a -> Graph a w -> Graph a w
-deleteVertex vertex = deleteVertex' <<< deleteIncidentEdges
+-- | Filter a graph, keeping the vertices which satisfy a predicate function.
+filter :: forall k v w. Ord k => (v -> Boolean) -> Graph k w v -> Graph k w v
+filter f graph = F.foldl filterVertex graph (keys graph)
   where
-    deleteVertex' = over Graph $ M.delete vertex
-    deleteIncidentEdges graph = F.foldl (\g v -> deleteEdge v vertex g) graph (adjacent vertex graph)
+    filterVertex g k = if f (unsafePartial $ fromJust $ lookup k graph)
+                         then g
+                         else deleteVertex k g
 
--- | Delete an edge from a graph.
-deleteEdge :: forall a w. (Ord a) => a -> a -> Graph a w -> Graph a w
-deleteEdge from to = over Graph $ M.alter deleteEdge' from
-  where deleteEdge' = maybe Nothing (Just <<< M.delete to)
+-- | Update a vertex in a graph using a function.
+update :: forall k v w. Ord k => (v -> Maybe v) -> k -> Graph k w v -> Graph k w v
+update f key graph = maybe graph (g <<< f) vertex
+  where
+    vertex :: Maybe v
+    vertex = lookup key graph
 
--- | Remove the matching vertices from a graph.
-filter :: forall a w. (Ord a) => (a -> Boolean) -> Graph a w -> Graph a w
-filter f graph = F.foldl filterVertex graph (vertices graph)
-  where filterVertex g v = if f v then deleteVertex v g else g
+    g :: Maybe v -> Graph k w v
+    g = maybe (deleteVertex key graph) (\v -> updateVertex key v graph)
--- test/Data/GraphSpec.purs
+++ test/Data/GraphSpec.purs
@@ -3,38 +3,33 @@ module Test.Data.GraphSpec where
 import Prelude
 
 import Data.Array (elem) as A
+import Data.Char (toLower) as C
 import Data.List (fromFoldable, (!!))
 import Data.Maybe (Maybe(..), fromJust)
-import Data.Map (empty, isEmpty) as M
-import Data.Newtype (wrap, unwrap)
 import Data.Tuple (Tuple(..))
 import Partial.Unsafe (unsafePartial)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-import Data.Graph (Graph, adjacent, connectedComponents, deleteEdge, deleteVertex, elem, empty, filter, fromAdjacencyList, insertEdge, insertVertex, isAdjacent, isEmpty, shortestPath, size, traverse, vertices, weight)
+import Data.Graph
 
 graphSpec :: forall r. (Spec r) Unit
 graphSpec = describe "Graph" do
   let edges = fromFoldable $
-    [ Tuple 'A' (fromFoldable [Tuple 'B' 1, Tuple 'C' 2])
-    , Tuple 'B' (fromFoldable [Tuple 'A' 1, Tuple 'D' 3])
-    , Tuple 'C' (fromFoldable [Tuple 'A' 2, Tuple 'D' 4])
-    , Tuple 'D' (fromFoldable [Tuple 'B' 3, Tuple 'C' 4])
-    , Tuple 'E' (fromFoldable [Tuple 'F' 1])
-    , Tuple 'F' (fromFoldable [Tuple 'E' 1])
-    , Tuple 'G' (fromFoldable [])
+    [ Tuple 1 (Tuple 'A' (fromFoldable [Tuple 2 1, Tuple 3 2]))
+    , Tuple 2 (Tuple 'B' (fromFoldable [Tuple 1 1, Tuple 4 3]))
+    , Tuple 3 (Tuple 'C' (fromFoldable [Tuple 1 2, Tuple 4 4]))
+    , Tuple 4 (Tuple 'D' (fromFoldable [Tuple 2 3, Tuple 3 4]))
+    , Tuple 5 (Tuple 'E' (fromFoldable [Tuple 6 1]))
+    , Tuple 6 (Tuple 'F' (fromFoldable [Tuple 5 1]))
+    , Tuple 7 (Tuple 'G' (fromFoldable []))
     ]
 
   let graph = fromAdjacencyList edges
 
-  describe "empty" do
-    it "returns an empty graph" do
-      M.isEmpty (unwrap $ empty :: Graph Int Int) `shouldEqual` true
-
   describe "isEmpty" do
-    it "returns an empty graph" do
-      isEmpty (wrap M.empty :: Graph Int Int) `shouldEqual` true
+    it "returns true if the graph is empty" do
+      isEmpty (empty :: Graph Int Int Int) `shouldEqual` true
 
   describe "vertices" do
     it "returns the vertices" do
@@ -44,111 +39,131 @@ graphSpec = describe "Graph" do
     it "returns the number of vertices" do
       size graph `shouldEqual` 7
 
-  describe "elem" do
+  describe "lookup" do
     it "returns true if the graph contains a vertex" do
-      elem 'A' graph `shouldEqual` true
+      lookup 1 graph `shouldEqual` Just 'A'
 
     it "returns false if the graph does not contain a vertex" do
-      elem 'H' graph `shouldEqual` false
+      lookup 8 graph `shouldEqual` Nothing
+
+  describe "member" do
+    it "returns true if the graph contains a vertex" do
+      member 1 graph `shouldEqual` true
+
+    it "returns false if the graph does not contain a vertex" do
+      member 8 graph `shouldEqual` false
 
   describe "adjacent" do
     it "returns the adjacent vertices given a vertex in the graph" do
-      adjacent 'A' graph `shouldEqual` fromFoldable ['B', 'C']
-      adjacent 'B' graph `shouldEqual` fromFoldable ['A', 'D']
-      adjacent 'C' graph `shouldEqual` fromFoldable ['A', 'D']
-      adjacent 'D' graph `shouldEqual` fromFoldable ['B', 'C']
-      adjacent 'E' graph `shouldEqual` fromFoldable ['F']
-      adjacent 'F' graph `shouldEqual` fromFoldable ['E']
+      adjacent 1 graph `shouldEqual` fromFoldable [2, 3]
+      adjacent 2 graph `shouldEqual` fromFoldable [1, 4]
+      adjacent 3 graph `shouldEqual` fromFoldable [1, 4]
+      adjacent 4 graph `shouldEqual` fromFoldable [2, 3]
+      adjacent 5 graph `shouldEqual` fromFoldable [6]
+      adjacent 6 graph `shouldEqual` fromFoldable [5]
 
     it "returns an empty list given a vertex with no edges" do
-      adjacent 'G' graph `shouldEqual` fromFoldable []
+      adjacent 7 graph `shouldEqual` fromFoldable []
 
     it "returns an empty list given a vertex not in the graph" do
-      adjacent 'H' graph `shouldEqual` fromFoldable []
+      adjacent 8 graph `shouldEqual` fromFoldable []
 
   describe "isAdjacent" do
     it "returns true if the vertices are adjacent" do
-      isAdjacent 'A' 'B' graph `shouldEqual` true
+      isAdjacent 1 2 graph `shouldEqual` true
 
     it "returns false if the vertices are not adjacent" do
-      isAdjacent 'A' 'D' graph `shouldEqual` false
+      isAdjacent 1 4 graph `shouldEqual` false
 
   describe "weight" do
     it "returns the weight of the edge between the vertices" do
-      weight 'A' 'B' graph `shouldEqual` Just 1
-      weight 'A' 'C' graph `shouldEqual` Just 2
-      weight 'B' 'D' graph `shouldEqual` Just 3
-      weight 'C' 'D' graph `shouldEqual` Just 4
+      weight 1 2 graph `shouldEqual` Just 1
+      weight 1 3 graph `shouldEqual` Just 2
+      weight 2 4 graph `shouldEqual` Just 3
+      weight 3 4 graph `shouldEqual` Just 4
 
     it "returns nothing if there is no edge between the vertices" do
-      weight 'A' 'D' graph `shouldEqual` Nothing
+      weight 1 4 graph `shouldEqual` Nothing
 
   describe "shortestPath" do
     it "returns the shortest path between the vertices" do
-      shortestPath 'A' 'A' graph `shouldEqual` Just (fromFoldable ['A'])
-      shortestPath 'A' 'B' graph `shouldEqual` Just (fromFoldable ['A', 'B'])
-      shortestPath 'A' 'C' graph `shouldEqual` Just (fromFoldable ['A', 'C'])
-      shortestPath 'A' 'D' graph `shouldEqual` Just (fromFoldable ['A', 'B', 'D'])
-      shortestPath 'D' 'A' graph `shouldEqual` Just (fromFoldable ['D', 'B', 'A'])
-      shortestPath 'D' 'B' graph `shouldEqual` Just (fromFoldable ['D', 'B'])
-      shortestPath 'D' 'C' graph `shouldEqual` Just (fromFoldable ['D', 'C'])
-      shortestPath 'D' 'D' graph `shouldEqual` Just (fromFoldable ['D'])
+      shortestPath 1 1 graph `shouldEqual` Just (fromFoldable [1])
+      shortestPath 1 2 graph `shouldEqual` Just (fromFoldable [1, 2])
+      shortestPath 1 3 graph `shouldEqual` Just (fromFoldable [1, 3])
+      shortestPath 1 4 graph `shouldEqual` Just (fromFoldable [1, 2, 4])
+      shortestPath 4 1 graph `shouldEqual` Just (fromFoldable [4, 2, 1])
+      shortestPath 4 2 graph `shouldEqual` Just (fromFoldable [4, 2])
+      shortestPath 4 3 graph `shouldEqual` Just (fromFoldable [4, 3])
+      shortestPath 4 4 graph `shouldEqual` Just (fromFoldable [4])
 
     it "returns nothing if there is no shortest path between the vertices" do
-      shortestPath 'A' 'E' graph `shouldEqual` Nothing
+      shortestPath 1 5 graph `shouldEqual` Nothing
 
   describe "traverse" do
     it "returns the vertices visited in a depth-first traversal of the graph" do
-      traverse 'A' graph `shouldEqual` fromFoldable ['A', 'B', 'D', 'C']
-      traverse 'B' graph `shouldEqual` fromFoldable ['B', 'A', 'C', 'D']
-      traverse 'C' graph `shouldEqual` fromFoldable ['C', 'A', 'B', 'D']
-      traverse 'D' graph `shouldEqual` fromFoldable ['D', 'B', 'A', 'C']
-      traverse 'E' graph `shouldEqual` fromFoldable ['E', 'F']
-      traverse 'F' graph `shouldEqual` fromFoldable ['F', 'E']
-      traverse 'G' graph `shouldEqual` fromFoldable ['G']
+      traverse 1 graph `shouldEqual` fromFoldable [1, 2, 4, 3]
+      traverse 2 graph `shouldEqual` fromFoldable [2, 1, 3, 4]
+      traverse 3 graph `shouldEqual` fromFoldable [3, 1, 2, 4]
+      traverse 4 graph `shouldEqual` fromFoldable [4, 2, 1, 3]
+      traverse 5 graph `shouldEqual` fromFoldable [5, 6]
+      traverse 6 graph `shouldEqual` fromFoldable [6, 5]
+      traverse 7 graph `shouldEqual` fromFoldable [7]
 
     it "returns an empty list given a vertex not in the graph" do
-      traverse 'H' graph `shouldEqual` fromFoldable []
+      traverse 8 graph `shouldEqual` fromFoldable []
 
   describe "connectedComponents" do
     it "returns the connected components of the graph" do
       let components = connectedComponents graph
-      vertices (unsafePartial $ fromJust $ components !! 0) `shouldEqual` fromFoldable ['A', 'B', 'C', 'D']
-      vertices (unsafePartial $ fromJust $ components !! 1) `shouldEqual` fromFoldable ['E', 'F']
-      vertices (unsafePartial $ fromJust $ components !! 2) `shouldEqual` fromFoldable ['G']
+      keys (unsafePartial $ fromJust $ components !! 0) `shouldEqual` fromFoldable [1, 2, 3, 4]
+      keys (unsafePartial $ fromJust $ components !! 1) `shouldEqual` fromFoldable [5, 6]
+      keys (unsafePartial $ fromJust $ components !! 2) `shouldEqual` fromFoldable [7]
 
   describe "insertVertex" do
     it "inserts a vertex into the graph" do
-      vertices (insertVertex 'H' graph) `shouldEqual` fromFoldable ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+      vertices (insertVertex 8 'H' graph) `shouldEqual` fromFoldable ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
   describe "insertEdge" do
     it "inserts an edge into the graph" do
-      let graph' = insertEdge 'F' 'G' 5 graph
-      weight 'F' 'G' graph' `shouldEqual` Just 5
+      let graph' = insertEdge 6 7 5 graph
+      weight 6 7 graph' `shouldEqual` Just 5
 
   describe "deleteVertex" do
     it "deletes a vertex from the graph" do
-      vertices (deleteVertex 'A' graph) `shouldEqual` fromFoldable ['B', 'C', 'D', 'E', 'F', 'G']
+      keys (deleteVertex 1 graph) `shouldEqual` fromFoldable [2, 3, 4, 5, 6, 7]
 
     it "returns the graph unchanged if there is no matching vertex" do
-      vertices (deleteVertex 'H' graph) `shouldEqual` fromFoldable ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+      keys (deleteVertex 8 graph) `shouldEqual` fromFoldable [1, 2, 3, 4, 5, 6, 7]
 
     it "deletes the edges incident on a vertex from the graph" do
-      let graph' = deleteVertex 'A' graph
-      isAdjacent 'A' 'B' graph' `shouldEqual` false
-      isAdjacent 'B' 'A' graph' `shouldEqual` false
+      let graph' = deleteVertex 1 graph
+      isAdjacent 1 2 graph' `shouldEqual` false
+      isAdjacent 2 1 graph' `shouldEqual` false
 
   describe "deleteEdge" do
     it "deletes an edge from the graph" do
-      let graph' = deleteEdge 'A' 'B' graph
-      isAdjacent 'A' 'B' graph' `shouldEqual` false
-      isAdjacent 'B' 'A' graph' `shouldEqual` true
+      let graph' = deleteEdge 1 2 graph
+      isAdjacent 1 2 graph' `shouldEqual` false
+      isAdjacent 2 1 graph' `shouldEqual` true
 
     it "returns the graph unchanged if there is no matching edge" do
-      let graph' = deleteEdge 'F' 'G' graph
-      isAdjacent 'F' 'G' graph' `shouldEqual` false
+      let graph' = deleteEdge 6 7 graph
+      isAdjacent 6 7 graph' `shouldEqual` false
+
+  describe "map" do
+    it "maps a function over the vertices of the graph" do
+      vertices (map C.toLower graph) `shouldEqual` fromFoldable ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
   describe "filter" do
-    it "removes matching vertices from the graph" do
-      let f vertex = A.elem vertex ['A', 'B', 'C']
-      vertices (filter f graph) `shouldEqual` fromFoldable ['D', 'E', 'F', 'G']
+    it "removes matching vertices of the graph" do
+      let f v = A.elem v ['A', 'B', 'C']
+      keys (filter f graph) `shouldEqual` fromFoldable [1, 2, 3]
+
+  describe "update" do
+    it "updates a vertex of the graph" do
+      let f = const $ Just 'H'
+      vertices (update f 1 graph) `shouldEqual` fromFoldable ['H', 'B', 'C', 'D', 'E', 'F', 'G']
+
+    it "deletes the vertex if the function returns Nothing" do
+      let f = const Nothing
+      vertices (update f 1 graph) `shouldEqual` fromFoldable ['B', 'C', 'D', 'E', 'F', 'G']
